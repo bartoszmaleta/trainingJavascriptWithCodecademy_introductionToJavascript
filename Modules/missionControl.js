b28a14e62bbda528de2ee1d2c64f922c9218a43c
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-08-06 12:35:40 +0200

    Add basic promise

--- Modules/missionControl.js
+++ Modules/missionControl.js
@@ -23,3 +23,28 @@ function displaySpeedRangeStatus() {
   });
 }
 displaySpeedRangeStatus();
+
+// SECOND OPTION, with aliases
+// import {aircrafts, flightReqs, meetsStaffReqs, meetsSpeedRangeReqs} from './airplane';
+
+// function displayFuelCapacity() {
+//   aircrafts.forEach(function(element) {
+//     console.log('Fuel Capacity of ' + element['name'] + ': ' + element['fuelCapacity']);
+//   });
+// }
+
+// displayFuelCapacity();
+
+// function displayStaffStatus() {
+//   aircrafts.forEach(function(element) {
+//       console.log(element.name + ' meets staff requirements: ' + meetsStaffReqs(element.availableStaff, flightReqs.requiredStaff));
+//   }); 
+// }
+// displayStaffStatus();
+
+// function displaySpeedRangeStatus() {
+//   aircrafts.forEach(function(element) {
+//     console.log(element.name + 'meets speed range requirements: ' + meetsSpeedRangeReqs(element.maxSpeed, element.minSpeed, flightReqs.requiredSpeedRange));
+//   });
+// }
+// displaySpeedRangeStatus();
